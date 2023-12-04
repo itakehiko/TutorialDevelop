dc26825c77c148855bdb5fbc7067cc5ffaaf15af
itakehiko <takehiko.iwasaki.24@gmail.com>
2023-12-04 21:29:19 +0900

    User変更

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,10 +4,7 @@ import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
@@ -32,6 +29,18 @@ public class UserController {
 
     @PostMapping("/register")
     public String postRegister(User user) {
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("user", service.getUser(id));
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
         service.saveUser(user);
         return "redirect:/user/list";
     }
